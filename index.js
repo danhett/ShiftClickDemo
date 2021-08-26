87dd14efddb26f2f53a4b68cfd0c5102a5baea36
Dan Hett <hellodanhett@gmail.com>
2021-08-26 11:56:54 +0100

    Added more stuff

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello, world!")
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/mycoolendpoint', (req, res) => {
+    res.send('<h1>ENDPOINT!</h1>')
+})
+
+app.get('/data', (req, res) => {
+    var myData = {
+        "some":"thing",
+        "other":12345
+    }
+
+    res.send(myData)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
